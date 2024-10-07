faecda240d24a265cbcfdcc96074fc353c12f235
robbinl <robbinl@telusplanet.net>
2024-10-07 16:34:01 -0600

    add Topic2

---
+++ b-Topic1-Intro to Micro/b-assessment-docs-specs/ICA03/BitWiseCS/BitWiseCS/BitWiseCS/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BitWiseCS
+{
+    class Program
+    {
+        //Masks Constants
+        const UInt16 BIT0 =  0x0001;
+        const UInt16 BIT15 = 0x8000;
+
+        static void Main(string[] args)
+        {
+            string input;
+            UInt16 number;
+
+            while(true)
+            {
+
+
+                Console.Write("Enter a positive number: ");
+                input = Console.ReadLine();
+                try
+                {
+                    number = UInt16.Parse(input);
+                    if( number < 0 ) 
+                    {
+                        throw new Exception("The number cannot be negative");
+                    }
+                    Console.WriteLine("The number in DEC: {0:D}", number);
+                    Console.WriteLine("The number in HEX: 0x{0:X}", number);
+                    Console.WriteLine("The number in BIN: {0}", ToBinary(number));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("The number you have entered is not valid");
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+
+                Console.Write("Press Any Key to repeat... ");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static string ToBinary(UInt16 _number)
+        {
+            //To be implemented using bitmasking
+            return "Number display as binary";
+        }
+    }
+}
---
+++ c-Topic2-GPIO-AtoDPort/a-resources/a-resources.md
@@ -0,0 +1,9 @@
+# Resources
+
+- [Basic Freq Measurement with the AD2 by Simon Walker](https://www.youtube.com/watch?v=N5jVD07FVYU)
+
+- [Compilation Unit Basics by Simon Walker](https://www.youtube.com/watch?v=1IvrSkqcs8s)
+
+- [ICA04 Math](./ICA04-Math.pdf)
+
+- [Switch LED Port Notes](./Switch-LED-Port-Notes.pdf)
